1250cdd30cf3841b79cdaf52beef97730a8d32fe
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-05-27 23:23:15 +0530

    almost done

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,9 +4,16 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
+    const handleAuthenticaton = () => {
+        if (user) {
+            signOut(auth);
+        }
+    };
     return (
         <div className="header">
             <Link to="/">
@@ -16,15 +23,22 @@ const Header = () => {
                 <input type="text" className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
             </div>
+
             <div className="header__nav">
-                <Link to="/login" style={{ textDecoration: "none" }}>
-                    <div className="header__option">
+                <Link to={user === null ? "/login" : "#"}>
+                    <div
+                        onClick={handleAuthenticaton}
+                        className="header__option"
+                    >
                         <span className="header__optionLineOne">
-                            Hello Guest
+                            Hello {!user ? "Guest" : user.email}
                         </span>
-                        <span className="header__optionLineTwo">Sign In</span>
+                        <span className="header__optionLineTwo">
+                            {user ? "Sign Out" : "Sign In"}
+                        </span>
                     </div>
                 </Link>
+
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
                     <span className="header__optionLineTwo">& Orders</span>
